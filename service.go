e9286b40c9706ef8a710041396be9e3107ed03cb
CakeAL <1742507190@qq.com>
2024-07-18 15:15:40 +0800

    feat(service): day3, 通过反射实现 service

---
+++ service.go
@@ -0,0 +1,116 @@
+package easy_rpc
+
+import (
+	"go/ast"
+	"log"
+	"reflect"
+	"sync/atomic"
+)
+
+type methodType struct {
+	method    reflect.Method
+	ArgType   reflect.Type
+	ReplyType reflect.Type
+	numCalls  uint64
+}
+
+// NumCalls 确保原子性前提下返回调用次数
+func (m *methodType) NumCalls() uint64 {
+	return atomic.LoadUint64(&m.numCalls)
+}
+
+// newArgv 创建新的方法入参
+func (m *methodType) newArgv() reflect.Value {
+	var argv reflect.Value
+	// arg may be a pointer type, or a value type
+	if m.ArgType.Kind() == reflect.Ptr {
+		// 是指针，创建该指针指向的元素类型的实例的指针
+		//（因为 m.ArgType 是指针，所以返回的 argv 也是指针）
+		argv = reflect.New(m.ArgType.Elem())
+	} else {
+		// 不是指针，创建该类型的实例的指针，使用 Elem() 获取其值
+		argv = reflect.New(m.ArgType).Elem()
+	}
+	return argv
+}
+
+// newReplyv 创建新的返回值
+func (m *methodType) newReplyv() reflect.Value {
+	// reply must be a pointer type
+	replyv := reflect.New(m.ReplyType.Elem())
+	// 返回值的类型
+	switch m.ReplyType.Elem().Kind() {
+	case reflect.Map:
+		// 是 Map，返回值设置为新的 Map 类型
+		replyv.Elem().Set(reflect.MakeMap(m.ReplyType.Elem()))
+	case reflect.Slice:
+		// 是 Slice，返回值设置为新的 Slice 类型
+		replyv.Elem().Set(reflect.MakeSlice(m.ReplyType.Elem(), 0, 0))
+	default:
+		panic("unhandled default case")
+	}
+	return replyv
+}
+
+type service struct {
+	name   string
+	typ    reflect.Type
+	rcvr   reflect.Value
+	method map[string]*methodType
+}
+
+// newService 创新新的服务
+func newService(rcvr interface{}) *service {
+	s := new(service)
+	s.rcvr = reflect.ValueOf(rcvr)
+	s.name = reflect.Indirect(s.rcvr).Type().Name()
+	s.typ = reflect.TypeOf(rcvr)
+	if !ast.IsExported(s.name) {
+		log.Fatalf("rpc server: %s is not a valid service name", s.name)
+	}
+	s.registerMethods()
+	return s
+}
+
+func (s *service) registerMethods() {
+	s.method = make(map[string]*methodType)
+	for i := 0; i < s.typ.NumMethod(); i++ {
+		method := s.typ.Method(i)
+		mType := method.Type
+		// 过滤符合条件的方法，正确的方法3个参数，一个返回值
+		if mType.NumIn() != 3 || mType.NumOut() != 1 {
+			continue
+		}
+		// 返回值应为 error
+		if mType.Out(0) != reflect.TypeOf((*error)(nil)).Elem() {
+			continue
+		}
+		// 第 0 个为 self
+		argType, replyType := mType.In(1), mType.In(2)
+		if !isExportedOrBuiltinType(argType) || !isExportedOrBuiltinType(replyType) {
+			continue
+		}
+		s.method[method.Name] = &methodType{
+			method:    method,
+			ArgType:   argType,
+			ReplyType: replyType,
+		}
+		log.Printf("rpc server: register %s.%s\n", s.name, method.Name)
+	}
+}
+
+func isExportedOrBuiltinType(t reflect.Type) bool {
+	return ast.IsExported(t.Name()) || t.PkgPath() == ""
+}
+
+func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
+	// 原子操作让调用次数加 1
+	atomic.AddUint64(&m.numCalls, 1)
+	f := m.method.Func
+	returnValues := f.Call([]reflect.Value{s.rcvr, argv, replyv})
+	// 返回了一个东西，肯定是 error
+	if errInter := returnValues[0].Interface(); errInter != nil {
+		return errInter.(error)
+	}
+	return nil
+}
