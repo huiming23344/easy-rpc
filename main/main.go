7135cfc172bf356fc022d912c2fc1747f037d1ef
CakeAL <1742507190@qq.com>
2024-07-20 23:28:04 +0800

    feat(all): day5, 支持 HTTP 协议

--- main/main.go
+++ main/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	easyrpc "easy-rpc"
 	"log"
 	"net"
+	"net/http"
 	"sync"
 	"time"
 )
@@ -19,26 +21,17 @@ func (f Foo) Sum(args Args, reply *int) error {
 	return nil
 }
 
-func startServer(addr chan string) {
+func startServer(addrCh chan string) {
 	var foo Foo
-	if err := easyrpc.Register(&foo); err != nil {
-		log.Fatal("register error:", err)
-	}
-	// pick a free port
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatal("Network error:", err)
-	}
-	log.Println("start rpc server on:", l.Addr())
-	addr <- l.Addr().String()
-	easyrpc.Accept(l)
+	l, _ := net.Listen("tcp", ":9999")
+	_ = easyrpc.Register(&foo)
+	easyrpc.HandleHTTP()
+	addrCh <- l.Addr().String()
+	_ = http.Serve(l, nil)
 }
 
-func main() {
-	log.SetFlags(0)
-	addr := make(chan string)
-	go startServer(addr)
-	client, _ := easyrpc.Dial("tcp", <-addr)
+func call(addrCh chan string) {
+	client, _ := easyrpc.DialHTTP("tcp", <-addrCh)
 	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
@@ -50,11 +43,18 @@ func main() {
 			defer wg.Done()
 			args := &Args{Num1: i, Num2: i * i}
 			var reply int
-			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
 			log.Printf("%d + %d = %d\n", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	addr := make(chan string)
+	go call(addr)
+	startServer(addr)
 }
